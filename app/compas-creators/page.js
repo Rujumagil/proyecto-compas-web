3da8b57089edf6e870b0798eee3dd166e703ff19
Rujumagil <proyectocompas.info@gmail.com>
2026-08-25 09:54:27 -0600

    seo: optimizar Compás Creators para productos digitales

--- app/compas-creators/page.js
+++ app/compas-creators/page.js
@@ -2,9 +2,24 @@ import ProductPage from "../components/product-page";
 import { products } from "../lib/products";
 
 export const metadata = {
-  title: "Compás Creators | Ideas convertidas en proyectos",
-  description: products.creators.description,
+  title: { absolute: products.creators.seoTitle },
+  description: products.creators.seoDescription,
   alternates: { canonical: "/compas-creators" },
+  openGraph: {
+    title: products.creators.seoTitle,
+    description: products.creators.seoDescription,
+    url: "https://www.proyectocompas.com/compas-creators",
+    siteName: "Compás Evolution",
+    images: [{ url: products.creators.ogImage, alt: products.creators.shortName }],
+    locale: "es_MX",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: products.creators.seoTitle,
+    description: products.creators.seoDescription,
+    images: [products.creators.ogImage],
+  },
 };
 
 export default function Page() {
