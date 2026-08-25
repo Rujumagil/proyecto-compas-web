be583b466f0ae7d54c59abab0f9b5b7cec741220
Rujumagil <proyectocompas.info@gmail.com>
2026-08-25 09:54:11 -0600

    seo: optimizar Compás One para búsquedas de CRM con IA

--- app/compas-one/page.js
+++ app/compas-one/page.js
@@ -2,9 +2,24 @@ import ProductPage from "../components/product-page";
 import { products } from "../lib/products";
 
 export const metadata = {
-  title: "Compás One | Operación inteligente",
-  description: products.one.description,
+  title: { absolute: products.one.seoTitle },
+  description: products.one.seoDescription,
   alternates: { canonical: "/compas-one" },
+  openGraph: {
+    title: products.one.seoTitle,
+    description: products.one.seoDescription,
+    url: "https://www.proyectocompas.com/compas-one",
+    siteName: "Compás Evolution",
+    images: [{ url: products.one.ogImage, alt: products.one.shortName }],
+    locale: "es_MX",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: products.one.seoTitle,
+    description: products.one.seoDescription,
+    images: [products.one.ogImage],
+  },
 };
 
 export default function Page() {
